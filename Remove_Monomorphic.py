e7b053b1018386cade3351f77b30a8a69d1bdad5
TomJKono <kono.yoshitaka@gmail.com>
2014-07-03 11:45:12 -0500

    Added new script
    Add a script to remove the monomorphic sites from a FASTA alignment

---
+++ Remove_Monomorphic.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+#   A script to remove monomorphic sites from a FASTA alignment.
+#   This is probably of limited use, but it still comes up every once
+#   in a while.
+#   Takes one argument:
+#       1) The FASTA alignment
+#   Takes one optional flag:
+#       -i omit gaps (indels)
+#   Outputs un-wrapped to stdout
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+#   Add the alignment as a required argument
+parser.add_argument('alignment', help='The FASTA alignment file')
+#   And add the optional indel flag
+parser.add_argument('-i', '--ignore-indels', help='Ignore gaps or indels', action='store_true')
+args = parser.parse_args()
+
+
+#   empty lists to hold the sequences and names from the FASTA file
+names = []
+sequences = []
+with open(args.alignment, 'r') as f:
+    for index, line in enumerate(f):
+        #   if the line starts with >, then it is a new sequence
+        if line.startswith('>'):
+            #   If we are at the first line of the file, then we don't have
+            #   any previously-parsed sequence to add
+            if index != 0:
+                sequences.append(seq)
+            seq = ''
+            name = line.strip()
+            names.append(name)
+        else:
+            #   Start accumulating the sequence
+            #   We have to do it this way because the sequence may be wrapped
+            #   which would throw off the line-by-line parsing
+            seq += line.strip()
+    #   Append the final seq of sequences
+    sequences.append(seq)
+
+#   Next, we transpose the list of sequences to start removing columns
+sequences_trans = zip(*sequences)
+polymorphic = []
+for column in sequences_trans:
+    #   we use a set() type since it automatically removes duplicate items
+    alleles = set(column)
+    #   If we only have one base at that site, then we omit it
+    if len(alleles) == 1:
+        continue
+    else:
+        #   If there's a gap character '-' in this set, we skip over it
+        #   but only if the -i flag is given
+        if args.ignore_indels:
+            if '-' in alleles:
+                continue
+            else:
+                polymorphic.append(column)
+        else:
+            polymorphic.append(column)
+
+#   Transpose it again to put them back in indivudal-rows
+polymorphic_trans = zip(*polymorphic)
+#   then print it out
+for seqname, seq in zip(names, polymorphic_trans):
+    print seqname
+    print ''.join(list(seq))
